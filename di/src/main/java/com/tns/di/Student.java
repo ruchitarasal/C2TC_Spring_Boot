fb11d46e091edd397d775ea03c1673979ff69197
abc <abc@DESKTOP-TK5HSIK>
2022-03-27 20:19:03 +0530

    Commit

---
+++ di/src/main/java/com/tns/di/Student.java
@@ -0,0 +1,20 @@
+package com.tns.di;
+
+public class Student 
+
+{
+	
+	private MathCheat chit;
+	
+
+	public void setChit(MathCheat chit) //Using setters
+	{
+		this.chit = chit;
+	}
+
+	void cheating()
+	{
+		chit.mathcheat();//Some other class
+	}
+
+}
