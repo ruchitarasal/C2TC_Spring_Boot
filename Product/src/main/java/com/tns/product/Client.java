ef1aa71a91829c4ec69a05c9b3b0b66ba66d6734
abc <abc@DESKTOP-TK5HSIK>
2022-04-02 20:57:15 +0530

    Commit

---
+++ Product/src/main/java/com/tns/product/Client.java
@@ -0,0 +1,14 @@
+package com.tns.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Client {
+
+	public static void main(String[] args) {
+	SpringApplication.run(Client.class, args);
+
+	}
+
+}
